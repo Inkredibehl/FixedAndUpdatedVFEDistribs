4e34d7609f254fb27b0b96f589a7f2f4112dd62b
Inkredibehl <154564710+Inkredibehl@users.noreply.github.com>
2024-04-17 02:11:46 +0200

    Add files via upload

---
+++ VFES_Distributions.lua
@@ -0,0 +1,144 @@
+require 'Items/ProceduralDistributions'
+
+
+
+table.insert(ProceduralDistributions.list.GunStoreCounter.items, "Base.762Clip10");
+table.insert(ProceduralDistributions.list.GunStoreCounter.items, 2);
+
+table.insert(ProceduralDistributions.list.GunStoreCounter.items, "Base.545Clip");
+table.insert(ProceduralDistributions.list.GunStoreCounter.items, 2);
+
+table.insert(ProceduralDistributions.list.GunStoreCounter.items, "Base.545Box");
+table.insert(ProceduralDistributions.list.GunStoreCounter.items, 5);
+
+table.insert(ProceduralDistributions.list.GunStoreCounter.items, "Base.762Clip102");
+table.insert(ProceduralDistributions.list.GunStoreCounter.items, 2.5);
+
+table.insert(ProceduralDistributions.list.GunStoreCounter.items, "Base.9mmClip8");
+table.insert(ProceduralDistributions.list.GunStoreCounter.items, 2.5);
+
+
+table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, "Base.PKM");
+table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, 0.5);
+
+table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, "Base.SVD");
+table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, 1);
+
+table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, "Base.Mosin");
+table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, 7);
+
+table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, "Base.Makarov");
+table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, 7);
+
+table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, "Base.SV98");
+table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, 1);
+
+table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, "Base.Saiga12");
+table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, 2);
+
+table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, "Base.12Clip5");
+table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, 3.5);
+
+table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, "Base.Vityaz");
+table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, 1);
+
+table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, "Base.Bizon");
+table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, 1);
+
+table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, "Base.PPSH");
+table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, 1);
+
+table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, "Base.AK104");
+table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, 1);
+
+table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, "Base.AK74");
+table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, 1);
+
+table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, "Base.AKS74U");
+table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, 1);
+
+table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, "Base.AK105");
+table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, 1);
+
+table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, "Base.VSS");
+table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, 0.7);
+
+
+table.insert(ProceduralDistributions.list.GunStoreShelf.items, "Base.762Clip10");
+table.insert(ProceduralDistributions.list.GunStoreShelf.items, 1);
+
+table.insert(ProceduralDistributions.list.GunStoreShelf.items, "Base.762Clip102");
+table.insert(ProceduralDistributions.list.GunStoreShelf.items, 1.5);
+
+table.insert(ProceduralDistributions.list.GunStoreShelf.items, "Base.9mmClip8");
+table.insert(ProceduralDistributions.list.GunStoreShelf.items, 2.5);
+
+table.insert(ProceduralDistributions.list.GunStoreShelf.items, "Base.12Clip5");
+table.insert(ProceduralDistributions.list.GunStoreShelf.items, 2);
+
+table.insert(ProceduralDistributions.list.GunStoreShelf.items, "Base.PPSHDrum");
+table.insert(ProceduralDistributions.list.GunStoreShelf.items, 2);
+
+table.insert(ProceduralDistributions.list.GunStoreShelf.items, "Base.545Clip");
+table.insert(ProceduralDistributions.list.GunStoreShelf.items, 2.5);
+
+table.insert(ProceduralDistributions.list.GunStoreShelf.items, "Base.545Box");
+table.insert(ProceduralDistributions.list.GunStoreShelf.items, 10);
+
+table.insert(ProceduralDistributions.list.GunStoreShelf.items, "Base.BizonClip");
+table.insert(ProceduralDistributions.list.GunStoreShelf.items, 1);
+
+table.insert(ProceduralDistributions.list.GunStoreShelf.items, "Base.45Clip25");
+table.insert(ProceduralDistributions.list.GunStoreShelf.items, 1);
+
+
+
+
+
+table.insert(ProceduralDistributions.list.PoliceStorageGuns.items, "Base.USP45");
+table.insert(ProceduralDistributions.list.PoliceStorageGuns.items, 4);
+
+table.insert(ProceduralDistributions.list.PoliceStorageGuns.items, "Base.45Clip12");
+table.insert(ProceduralDistributions.list.PoliceStorageGuns.items, 4);
+
+table.insert(ProceduralDistributions.list.PoliceStorageGuns.items, "Base.HK416");
+table.insert(ProceduralDistributions.list.PoliceStorageGuns.items, 0.5);
+
+table.insert(ProceduralDistributions.list.PoliceStorageGuns.items, "Base.M249");
+table.insert(ProceduralDistributions.list.PoliceStorageGuns.items, 0.05);
+
+table.insert(ProceduralDistributions.list.PoliceStorageGuns.items, "Base.45Clip25");
+table.insert(ProceduralDistributions.list.PoliceStorageGuns.items, 1);
+
+table.insert(ProceduralDistributions.list.PoliceStorageGuns.items, "Base.Makarov");
+table.insert(ProceduralDistributions.list.PoliceStorageGuns.items, 2);
+
+
+table.insert(ProceduralDistributions.list.ArmyStorageGuns.items, "Base.HK416");
+table.insert(ProceduralDistributions.list.ArmyStorageGuns.items, 1);
+
+table.insert(ProceduralDistributions.list.ArmyStorageGuns.items, "Base.M249");
+table.insert(ProceduralDistributions.list.ArmyStorageGuns.items, 0.2);
+
+table.insert(ProceduralDistributions.list.ArmyStorageGuns.items, "Base.USP45");
+table.insert(ProceduralDistributions.list.ArmyStorageGuns.items, 1);
+
+table.insert(ProceduralDistributions.list.ArmyStorageGuns.items, "Base.45Clip12");
+table.insert(ProceduralDistributions.list.ArmyStorageGuns.items, 1);
+
+table.insert(ProceduralDistributions.list.ArmyStorageGuns.items, "Base.VSS");
+table.insert(ProceduralDistributions.list.ArmyStorageGuns.items, 0.7);
+
+
+
+table.insert(ProceduralDistributions.list.PlankStashGun.items, "Base.Galil");
+table.insert(ProceduralDistributions.list.PlankStashGun.items, 1);
+
+table.insert(ProceduralDistributions.list.PlankStashGun.items, "Base.AKS74U");
+table.insert(ProceduralDistributions.list.PlankStashGun.items, 1.5);
+
+table.insert(ProceduralDistributions.list.PlankStashGun.items, "Base.AK105");
+table.insert(ProceduralDistributions.list.PlankStashGun.items, 1);
+
+table.insert(ProceduralDistributions.list.PlankStashGun.items, "Base.AK104");
+table.insert(ProceduralDistributions.list.PlankStashGun.items, 1);
---
+++ zRe_VFEStalker_Distributions.lua
@@ -0,0 +1,102 @@
+require 'Items/ProceduralDistributions'
+
+
+table.insert(ProceduralDistributions.list.GunStoreCounter.items, "Base.939Clip10");
+table.insert(ProceduralDistributions.list.GunStoreCounter.items, 2);
+
+table.insert(ProceduralDistributions.list.GunStoreCounter.items, "Base.556box100");
+table.insert(ProceduralDistributions.list.GunStoreCounter.items, 2);
+
+table.insert(ProceduralDistributions.list.GunStoreCounter.items, "Base.762box100");
+table.insert(ProceduralDistributions.list.GunStoreCounter.items, 2);
+
+table.insert(ProceduralDistributions.list.GunStoreCounter.items, "Base.12Clip5");
+table.insert(ProceduralDistributions.list.GunStoreCounter.items, 2);
+
+table.insert(ProceduralDistributions.list.GunStoreCounter.items, "Base.762Clip103");
+table.insert(ProceduralDistributions.list.GunStoreCounter.items, 2);
+
+table.insert(ProceduralDistributions.list.GunStoreCounter.items, "Base.9mmClip302");
+table.insert(ProceduralDistributions.list.GunStoreCounter.items, 2);
+
+table.insert(ProceduralDistributions.list.GunStoreCounter.items, "Base.45Clip12");
+table.insert(ProceduralDistributions.list.GunStoreCounter.items, 2);
+
+table.insert(ProceduralDistributions.list.GunStoreCounter.items, "Base.45Clip25");
+table.insert(ProceduralDistributions.list.GunStoreCounter.items, 2);
+
+
+
+table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, "Base.Galil");
+table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, 1);
+
+table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, "Base.UMP45");
+table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, 1);
+
+table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, "Base.USP45");
+table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, 1);
+
+table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, "Base.M249");
+table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, 1);
+
+table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, "Base.HK416");
+table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, 1);
+
+table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, "Base.SVT40");
+table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, 1);
+
+table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, "Base.MP153");
+table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, 1);
+
+table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, "Base.MP153sawn");
+table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, 1);
+
+
+table.insert(ProceduralDistributions.list.PoliceStorageGuns.items, "Base.556box100");
+table.insert(ProceduralDistributions.list.PoliceStorageGuns.items, 1.1);
+
+table.insert(ProceduralDistributions.list.PoliceStorageGuns.items, "Base.762box100");
+table.insert(ProceduralDistributions.list.PoliceStorageGuns.items, 1.1);
+
+table.insert(ProceduralDistributions.list.PoliceStorageGuns.items, "Base.762Clip10");
+table.insert(ProceduralDistributions.list.PoliceStorageGuns.items, 1.1);
+
+table.insert(ProceduralDistributions.list.PoliceStorageGuns.items, "Base.12Clip5");
+table.insert(ProceduralDistributions.list.PoliceStorageGuns.items, 1.1);
+
+table.insert(ProceduralDistributions.list.PoliceStorageGuns.items, "Base.Saiga12");
+table.insert(ProceduralDistributions.list.PoliceStorageGuns.items, 1);
+
+table.insert(ProceduralDistributions.list.PoliceStorageGuns.items, "Base.PKM");
+table.insert(ProceduralDistributions.list.PoliceStorageGuns.items, 1);
+
+table.insert(ProceduralDistributions.list.PoliceStorageGuns.items, "Base.SVD");
+table.insert(ProceduralDistributions.list.PoliceStorageGuns.items, 1);
+
+table.insert(ProceduralDistributions.list.PoliceStorageGuns.items, "Base.MP153");
+table.insert(ProceduralDistributions.list.PoliceStorageGuns.items, 1);
+
+table.insert(ProceduralDistributions.list.PoliceStorageGuns.items, "Base.MP153sawn");
+table.insert(ProceduralDistributions.list.PoliceStorageGuns.items, 1);
+
+
+table.insert(ProceduralDistributions.list.ArmyStorageGuns.items, "Base.556box100");
+table.insert(ProceduralDistributions.list.ArmyStorageGuns.items, 1.5);
+
+table.insert(ProceduralDistributions.list.ArmyStorageGuns.items, "Base.762box100");
+table.insert(ProceduralDistributions.list.ArmyStorageGuns.items, 1.5);
+
+table.insert(ProceduralDistributions.list.ArmyStorageGuns.items, "Base.762Clip10");
+table.insert(ProceduralDistributions.list.ArmyStorageGuns.items, 1.5);
+
+table.insert(ProceduralDistributions.list.ArmyStorageGuns.items, "Base.12Clip5");
+table.insert(ProceduralDistributions.list.ArmyStorageGuns.items, 1.5);
+
+table.insert(ProceduralDistributions.list.ArmyStorageGuns.items, "Base.Saiga12");
+table.insert(ProceduralDistributions.list.ArmyStorageGuns.items, 1);
+
+table.insert(ProceduralDistributions.list.ArmyStorageGuns.items, "Base.PKM");
+table.insert(ProceduralDistributions.list.ArmyStorageGuns.items, 1);
+
+table.insert(ProceduralDistributions.list.ArmyStorageGuns.items, "Base.SVD");
+table.insert(ProceduralDistributions.list.ArmyStorageGuns.items, 1);
